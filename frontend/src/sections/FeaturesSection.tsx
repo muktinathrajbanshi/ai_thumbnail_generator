c23aae1bb680fa72b4da6165ac3e64e4ed73c318
unknown <muktinathrajbanshi786@gmail.com>
2026-03-30 18:33:48 +0545

    now i am working on testimonial section ok

--- frontend/src/sections/FeaturesSection.tsx
+++ frontend/src/sections/FeaturesSection.tsx
@@ -60,7 +60,7 @@ export default function FeaturesSection() {
                         <h3 className="text-[24px]/7.5 text-slate-300 font-medium mt-6">boost your views with AI-optimized designs </h3>
                         <p className="text-slate-300 mt-2">Stop guessing and start ranking. Our AI creates designs proven to capture attention.</p>
                         <a href="https://prebuiltui.com" className="group flex items-center gap-2 mt-4 text-pink-600 hover:text-pink-700 transition">
-                            Learn more about the product
+                            Start generating free
                             <ArrowUpRight className="size-5 group-hover:translate-x-0.5 transition duration-300" />
                         </a>
                     </motion.div>
