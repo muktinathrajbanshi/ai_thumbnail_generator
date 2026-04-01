2e9a8160904877186c94e60baeb54d7b1918a6e9
unknown <muktinathrajbanshi786@gmail.com>
2026-04-01 10:46:00 +0545

    adding necessary useState hooks for generate page

--- frontend/src/pages/Generate.tsx
+++ frontend/src/pages/Generate.tsx
@@ -1,5 +1,17 @@
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+// import type  { IThumbnail } from "../assets/assets";
 
 const Generate = () => {
+
+  const {id} = useParams();
+  const [title, setTitle] = useState("")
+  const [additionalDetails, setAdditionalDetails] = useState("")
+
+  // const [thumbnail, setThumbnail] = useState<IThumbnail | null>(null)
+
+  const [loading, setLoading] = useState(false)
+
   return (
     <div>
       
