9a0bc951b42f35f2c80a7eafd004bafc8088eb5a
unknown <muktinathrajbanshi786@gmail.com>
2026-04-02 17:47:11 +0545

    uski maki line break vayera tanab rata vanchhu kaha error chha vanera

--- frontend/src/components/AspectRatioSelector.tsx
+++ frontend/src/components/AspectRatioSelector.tsx
@@ -1,5 +1,5 @@
 import { RectangleHorizontal, RectangleVertical, Square } from "lucide-react";
-import {aspectRatios, type AspectRatio} from "../assets/assets"
+import { aspectRatios, type AspectRatio } from "../assets/assets";
 
 const AspectRatioSelector = ({value, onChange} : {value:
     AspectRatio; onChange: (ratio: AspectRatio) => void}) => {
@@ -20,19 +20,17 @@ const AspectRatioSelector = ({value, onChange} : {value:
                 const selected = value === ratio;
 
                 return (
-                    <button key={ratio} type="button" onClick={() 
-                        => onChange(ratio) 
+                    <button key={ratio} type="button" onClick={() =>onChange(ratio)} 
                         className={`flex items-center gap-2 rounded-md 
-                        border px-5 py-2.5 text-sm transition border-white/
-                        10 ${selected? "bg-white/10" :"hover:bg-white/6"}`}
-                        >
+                        border px-5 py-2.5 text-sm transition border-white/10 
+                        ${selected? "bg-white/10" :"hover:bg-white/6"}`}
+                    >
                         {iconMap[ratio]}
                         <span className="tracking-widest">{ratio}</span>
                     </button>
                 )
             })}
         </div>
-
     </div>
   )
 }
