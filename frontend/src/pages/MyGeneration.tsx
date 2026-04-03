01602d7f9747166cb87ae09c2da0ef3d69ee4e3e
unknown <muktinathrajbanshi786@gmail.com>
2026-04-03 20:39:58 +0545

    hait chhito

--- frontend/src/pages/MyGeneration.tsx
+++ frontend/src/pages/MyGeneration.tsx
@@ -5,7 +5,11 @@ const MyGeneration = () => {
     <>
      <SoftBackdrop /> 
      <div className="mt-32 min-h-screen px-6 md:px-16 lg:px-24 xl:px-32">
-
+      {/* Header  */}
+      <div>
+        <h1>My Generation</h1>
+        <p>View and manage all your AI-generated thumbnails</p>
+      </div>
      </div>
     </>
   )
