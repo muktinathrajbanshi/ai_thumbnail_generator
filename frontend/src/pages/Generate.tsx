045cd844c5e1251172d3fa5a67c7b28f266c3a29
unknown <muktinathrajbanshi786@gmail.com>
2026-04-03 14:20:01 +0545

    aile lai yat garum hola yar

--- frontend/src/pages/Generate.tsx
+++ frontend/src/pages/Generate.tsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import SoftBackdrop from "../components/SoftBackdrop";
-import { colorSchemes, type AspectRatio, type IThumbnail, type ThumbnailStyle } from "../assets/assets";
+import { colorSchemes, dummyThumbnails, type AspectRatio, type IThumbnail, 
+  type ThumbnailStyle } from "../assets/assets";
 import AspectRatioSelector from "../components/AspectRatioSelector";
 import StyleSelector from "../components/StyleSelector";
 import ColorSchemeSelector from "../components/ColorSchemeSelector";
@@ -22,6 +23,28 @@ const Generate = () => {
 
   const [styleDropdownOpen, setStyleDropdownOpen] = useState(false)
 
+  const handleGenerate = async () => {
+
+  }
+
+  const fetchThumbnail = async () => {
+    if(id){
+      const thumbnail : any = dummyThumbnails.find((thumbnail) => thumbnail._id === id);
+      setThumbnail(thumbnail)
+      setAdditionalDetails(thumbnail.user_prompt)
+      setTitle(thumbnail.title)
+      setColorSchemeId(thumbnail.color_scheme)
+      setAspectRatio(thumbnail.aspect_ratio)
+      setStyle(thumbnail.style)
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    if(id) {
+      fetchThumbnail()
+    }
+  }, [id])
   
   return (
     <>
@@ -73,7 +96,7 @@ const Generate = () => {
                 </div>
                 {/* Button  */}
                 {!id && (
-                  <button className="text-[15px] w-full py-3.5 rounded-xl font-medium bg-linear-to-b
+                  <button onClick={handleGenerate} className="text-[15px] w-full py-3.5 rounded-xl font-medium bg-linear-to-b
                   from-pink-500 to-pink-600 hover:from-pink-700 disabled:cursor-not-allowed 
                   transition-colors">
                     {loading ? "Generating..." : "Generate Thumbnail"}
