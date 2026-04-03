332c4b259966f600e8f034185ef8721e42d244c9
unknown <muktinathrajbanshi786@gmail.com>
2026-04-03 13:59:18 +0545

    Oh my god outstanding preview yar

--- frontend/src/pages/Generate.tsx
+++ frontend/src/pages/Generate.tsx
@@ -5,6 +5,7 @@ import { colorSchemes, type AspectRatio, type IThumbnail, type ThumbnailStyle } 
 import AspectRatioSelector from "../components/AspectRatioSelector";
 import StyleSelector from "../components/StyleSelector";
 import ColorSchemeSelector from "../components/ColorSchemeSelector";
+import PreviewPanel from "../components/PreviewPanel";
 
 const Generate = () => {
 
@@ -82,7 +83,12 @@ const Generate = () => {
               </div>
             </div>
             {/* Right Panel  */}
-            <div></div>
+            <div>
+              <div className="p-6 rounded-2xl bg-white/8 border border-white/10 shadow-xl">
+                <h2 className="text-lg font-semibold text-zinc-100 mb-4">Preview</h2>
+                <PreviewPanel thumbnail={thumbnail} isLoading={loading} aspectRatio={aspectRatio} />
+              </div>
+            </div>
           </div>
         </main>
       </div>
