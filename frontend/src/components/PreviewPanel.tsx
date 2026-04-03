0dcfe14958c32293b0af13f18da7ebd93dc979cb
unknown <muktinathrajbanshi786@gmail.com>
2026-04-03 20:16:16 +0545

    one mistake game over

--- frontend/src/components/PreviewPanel.tsx
+++ frontend/src/components/PreviewPanel.tsx
@@ -35,7 +35,7 @@ const PreviewPanel = ({thumbnail, isLoading, aspectRatio} : {thumbnail: IThumbna
                 <img src={thumbnail?.image_url} alt={thumbnail.title}
                 className="h-full w-full object-cover" />
                 
-                <div className="absolute inset-0 flex item-end justify-center
+                <div className="absolute inset-0 flex items-end justify-center
                  bg-black/10 opacity-0 transition-opacity group-hover:opacity-100">
                     <button onClick={onDownload} type="button" className="mb-6 flex items-center
                     gap-2 rounded-md px-5 py-2.5 text-xs font-medium transition
