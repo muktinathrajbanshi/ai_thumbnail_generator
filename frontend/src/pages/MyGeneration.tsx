416eac82f2dd6eac8a03534745c1dadcf1253424
unknown <muktinathrajbanshi786@gmail.com>
2026-04-03 20:41:58 +0545

    aja lai yati la

--- frontend/src/pages/MyGeneration.tsx
+++ frontend/src/pages/MyGeneration.tsx
@@ -6,9 +6,9 @@ const MyGeneration = () => {
      <SoftBackdrop /> 
      <div className="mt-32 min-h-screen px-6 md:px-16 lg:px-24 xl:px-32">
       {/* Header  */}
-      <div>
-        <h1>My Generation</h1>
-        <p>View and manage all your AI-generated thumbnails</p>
+      <div className="mb-8">
+        <h1 className="text-2xl font-bold text-zinc-200">My Generation</h1>
+        <p className="text-sm text-zinc-400 mt-1">View and manage all your AI-generated thumbnails</p>
       </div>
      </div>
     </>
