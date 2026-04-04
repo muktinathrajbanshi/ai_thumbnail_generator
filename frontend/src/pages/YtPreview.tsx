aa624a8133b9ef719ee0ac698ec3769c90b7ce86
unknown <muktinathrajbanshi786@gmail.com>
2026-04-04 17:30:22 +0545

    hait aja lai yati la

--- frontend/src/pages/YtPreview.tsx
+++ frontend/src/pages/YtPreview.tsx
@@ -1,5 +1,13 @@
+import { useSearchParams } from "react-router-dom";
+import { yt_html } from "../assets/assets"
 
 const YtPreview = () => {
+
+  const [searchParams] = useSearchParams()
+
+  const thumbnail_url = searchParams.get("thumbnail_url")
+  const title = searchParams.get("title")
+
   return (
     <div>
       
