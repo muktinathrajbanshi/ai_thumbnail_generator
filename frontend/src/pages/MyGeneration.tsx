1db032eef8fb63b320f1c9e29339fbc6f879a6ac
unknown <muktinathrajbanshi786@gmail.com>
2026-04-04 08:57:29 +0545

    wow perfect animation for grid and layout

--- frontend/src/pages/MyGeneration.tsx
+++ frontend/src/pages/MyGeneration.tsx
@@ -5,11 +5,19 @@ import SoftBackdrop from "../components/SoftBackdrop";
 const MyGeneration = () => {
 
   const [thumbnails, setThumbnails] = useState<IThumbnail>([])
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(true)
 
   const fetchThumbnails = async () => {
     setThumbnails(dummyThumbnails as unknown as IThumbnail[])
-    setLoading(false)
+    // setLoading(false)
+  }
+
+  const handleDownload = (image_url: string) => {
+    window.open(image_url, "_blank")
+  }
+
+  const handleDelete = async (id: string) => {
+    console.log(id);
   }
 
   useEffect(() => {
@@ -25,6 +33,15 @@ const MyGeneration = () => {
         <h1 className="text-2xl font-bold text-zinc-200">My Generation</h1>
         <p className="text-sm text-zinc-400 mt-1">View and manage all your AI-generated thumbnails</p>
       </div>
+
+      {/* Loading  */}
+      {loading && (
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+          {Array.from({length: 6}).map((_, i) =>(
+            <div key={i} className="rounded-2xl bg-white/6 border border-white/10 animate-pulse h-[260px]" />
+          ))}
+        </div>
+      )}
      </div>
     </>
   )
