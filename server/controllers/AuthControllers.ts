a27208a128f4e6d92a6e1e89316b351fa7eb10f3
unknown <muktinathrajbanshi786@gmail.com>
2026-04-06 17:45:31 +0545

    hait khatra, khatra mane danger

--- server/controllers/AuthControllers.ts
+++ server/controllers/AuthControllers.ts
@@ -1,4 +1,6 @@
 import { Request, Response } from "express";
+import User from "../models/User.js";
+import bcrypt from "bcrypt";
 
 
 // Controllers For User Registration 
@@ -6,6 +8,16 @@ import { Request, Response } from "express";
 export const registerUser = async (req: Request, res: Response) => {
     try {
         const {name, email, password} = req.body;
+
+        // find user by email 
+        const user = await User.findOne({email});
+        if(user){
+            return res.status(400).json({message: "User already exists"})
+        }
+
+        // Encrypt the password 
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt)
     } catch (error) {
         
     }
