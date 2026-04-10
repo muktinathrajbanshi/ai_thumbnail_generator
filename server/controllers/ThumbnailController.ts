e45c5291a4fa416b9489b00d0e8d4d1d93a1d613
unknown <muktinathrajbanshi786@gmail.com>
2026-04-10 14:48:44 +0545

    wow muktinath that's great job

--- server/controllers/ThumbnailController.ts
+++ server/controllers/ThumbnailController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import Thumbnail from "../models/Thumbnail.js";
+import { GenerateContentConfig } from "@google/genai";
 
 export const generateThumbnail = async (req: Request, res: Response) => {
     try {
@@ -20,6 +21,22 @@ export const generateThumbnail = async (req: Request, res: Response) => {
                 isGenerating: true
             })
             
+            const model = "gemini-3-pro-image-preview";
+            
+            const generationConfig: GenerateContentConfig = {
+                maxOutputTokens: 32768,
+                temperature: 1,
+                topP: 0.95,
+                responseModalities: ["IMAGE"],
+                imageConfig: {
+                    aspectRatio: "16:9",
+                    imageSize: "1K"
+                },
+                safetySettings: [
+                    
+                ]
+            }
+
     } catch (error) {
         
     }
