a11aa5fe2fc4adb684c9689358a2068a3fe19874
unknown <muktinathrajbanshi786@gmail.com>
2026-04-12 12:53:14 +0545

    necessary packages are installed

--- server/controllers/UserController.ts
+++ server/controllers/UserController.ts
@@ -19,8 +19,13 @@ export const getUsersThumbnails = async (req: Request, res: Response) => {
 // Controllers to get single Thumbnail of a User
 export const getThumbnailbyId = async (req: Request, res: Response) => {
     try {
-        
-    } catch (error) {
+        const {userId} = req.session;
+        const { id } = req.params;
+
+        const thumbnail = await Thumbnail.findOne({userId, _id: id});
+        res.json({thumbnail})
+
+    } catch (error: any) {
         console.log(error);
         res.status(500).json({ message: error.message });
     }
