37d324085b172a344cf6c3aa438647519b4f1ec4
unknown <muktinathrajbanshi786@gmail.com>
2026-04-12 14:23:07 +0545

    mummy app imteham ki ghadi aagai hain

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -58,9 +58,15 @@ export default function Navbar() {
             <div className={`fixed inset-0 z-100 bg-black/40 backdrop-blur flex flex-col items-center justify-center text-lg gap-8 md:hidden transition-transform duration-400 ${isOpen ? "translate-x-0" : "-translate-x-full"}`}>
                 <Link onClick={() => setIsOpen(false)} to="/">Home</Link>
                 <Link onClick={() => setIsOpen(false)} to="/generate">Generate</Link>
+                {isLoggedIn ? 
                 <Link onClick={() => setIsOpen(false)} to="/my-generation">My Generations</Link>
-                <Link onClick={() => setIsOpen(false)} to="#">My Contact</Link>
-                <Link onClick={() => setIsOpen(false)} to="/login">Login</Link>
+                : <Link onClick={() => setIsOpen(false)} to="#">About</Link>}
+                
+                <Link onClick={() => setIsOpen(false)} to="#">Contact us</Link>
+                {isLoggedIn
+                ? <button onClick={() => {setIsOpen(false); logout()}}>Logout</button>
+                : <Link onClick={() => setIsOpen(false)} to="/login">Login</Link>}
+                
 
                 <button onClick={() => setIsOpen(false)} className="active:ring-3 active:ring-white aspect-square size-10 p-1 items-center justify-center bg-pink-600 hover:bg-pink-700 transition text-white rounded-md flex">
                     <XIcon />
