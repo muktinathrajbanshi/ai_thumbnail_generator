244d72f707dc3442416405c919de4e90e7b92ee8
unknown <muktinathrajbanshi786@gmail.com>
2026-04-12 15:10:26 +0545

    wow that's impressive muktinath

--- frontend/src/pages/Generate.tsx
+++ frontend/src/pages/Generate.tsx
@@ -9,6 +9,7 @@ import ColorSchemeSelector from "../components/ColorSchemeSelector";
 import PreviewPanel from "../components/PreviewPanel";
 import { useAuth } from "../context/AuthContext";
 import toast from "react-hot-toast";
+import api from "../configs/api";
 
 const Generate = () => {
 
@@ -43,27 +44,46 @@ const Generate = () => {
       text_overlay: true,
     }
 
-    
+    const {data} = await api.post("/api/thumbnail/generate", api_payload);
+    if(data.thumbnail) {
+      navigate("/generate/" + data.thumbnail._id);
+      toast.success(data.message)
+    }
   }
 
   const fetchThumbnail = async () => {
-    if(id){
-      const thumbnail : any = dummyThumbnails.find((thumbnail) => thumbnail._id === id);
-      setThumbnail(thumbnail)
-      setAdditionalDetails(thumbnail.user_prompt)
-      setTitle(thumbnail.title)
-      setColorSchemeId(thumbnail.color_scheme)
-      setAspectRatio(thumbnail.aspect_ratio)
-      setStyle(thumbnail.style)
-      setLoading(false)
+    try {
+      const { data } = await api.get(`/api/user/thumbnail/${id}`);
+      setThumbnail(data?.thumbnail as IThumbnail);
+      setLoading(!data?.thumbnail?.image_url);
+      setAdditionalDetails(data?.thumbnail?.user_prompt)
+      setTitle(data?.thumbnail?.title)
+      setColorSchemeId(data?.thumbnail?.color_scheme)
+      setAspectRatio(data?.thumbnail?.aspect_ratio)
+      setStyle(data?.thumbnail?.style)
+    } catch (error: any) {
+      console.log(error);
+      toast.error(error?.response?.data?.message || error.message)
     }
   }
 
   useEffect(() => {
-    if(id) {
+    if(isLoggedIn && id) {
       fetchThumbnail()
     }
-  }, [id])
+    if(id && loading && isLoggedIn) {
+      const interval = setInterval(() => {
+        fetchThumbnail()
+      }, 5000);
+      return () => clearInterval(interval)
+    }
+  }, [id, loading, isLoggedIn])
+
+  useEffect(() => {
+    if(!id && thumbnail) {
+      setThumbnail(null)
+    }
+  }, [pathname])
   
   return (
     <>
