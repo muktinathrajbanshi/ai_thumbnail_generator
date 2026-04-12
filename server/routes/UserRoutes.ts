646a734befeee91c7511afa22444e6e785ea333f
unknown <muktinathrajbanshi786@gmail.com>
2026-04-12 12:43:31 +0545

    let's get started muktinath

--- server/routes/UserRoutes.ts
+++ server/routes/UserRoutes.ts
@@ -1,9 +1,10 @@
 import  Express  from "express";
 import { getThumbnailbyId, getUsersThumbnails } from "../controllers/UserController.js";
+import protect from "../middlewares/auth.js";
 
 const UserRouter = Express.Router();
 
-UserRouter.get("/thumbnails", getUsersThumbnails)
-UserRouter.get("/thumbnail/:id", getThumbnailbyId)
+UserRouter.get("/thumbnails", protect, getUsersThumbnails)
+UserRouter.get("/thumbnail/:id", protect, getThumbnailbyId)
 
 export default UserRouter;
