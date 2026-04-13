1939b11f9167990a669d9a8359b0efc67c88c1b0
unknown <muktinathrajbanshi786@gmail.com>
2026-04-13 14:59:37 +0545

    due to dot point there is occurred serious matter

--- frontend/src/pages/MyGeneration.tsx
+++ frontend/src/pages/MyGeneration.tsx
@@ -142,8 +142,7 @@ const MyGeneration = () => {
                     className="size-6 bg-black/50 p-1 rounded
                     hover:bg-pink-600 transition-all" />
 
-                    <Link target="_blank" to={`/preview?thumbnail_url=${thumb.
-                      image_url}&title=${thumb.title}`}>
+                    <Link target="_blank" to={`/preview?thumbnail_url=${thumb.image_url}&title=${thumb.title}`}>
                         
                     <ArrowUpRightIcon className="size-6 bg-black/50 p-1 rounded
                     hover:bg-pink-600 transition-all" />
