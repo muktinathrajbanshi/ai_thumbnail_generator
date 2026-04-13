cb91d733ba0e7c11d811043818efde3017eff7ea
unknown <muktinathrajbanshi786@gmail.com>
2026-04-13 08:50:59 +0545

    halka kaam baki thiyo so updated cors middleware

--- server/server.ts
+++ server/server.ts
@@ -22,7 +22,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:3000"],
+    origin: ["http://localhost:5173", "http://localhost:3000", "https://hiloveyou.vercel.app"],
     credentials: true
 }))
 
